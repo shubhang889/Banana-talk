eca63c92558ffa7c375c6497ae0ad9e22ae337ab
Shubhang Agrawal <shubhang.agrawal@halodoc.com>
2021-02-08 23:20:52 +0530

    completed the application
    Enter text in English to translate it into minion language

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
- 
+var translateButton = document.querySelector("#translate-button");
+translateButton.addEventListener("click", clickHandler)
+
+var translateInput = document.querySelector("#translate-input");
+
+var translateOutput = document.querySelector("#translate-output");
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function clickHandler(event) {
+
+    var input = translateInput.value;
+
+    var finalURL = constructURL(input);
+    
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            translateOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("error occured"))
+    
+}
+
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
+}
